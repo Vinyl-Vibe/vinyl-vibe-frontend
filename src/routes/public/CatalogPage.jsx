41f4c665668d08d4fe679a190d24f7a27e89c143
Damian <hello@damianpetrov.com>
2024-12-03 23:51:14 +1100

    style: homepage

--- src/routes/public/CatalogPage.jsx
+++ src/routes/public/CatalogPage.jsx
@@ -79,7 +79,6 @@ function CatalogPage() {
 
 	return (
 		<div>
-			<MainNav />
 			<main className="mx-auto max-w-7xl pt-[calc(5rem-1px)]">
 				<div className="px-6 pb-6 pt-40 border">
 					<h1 className="text-5xl font-medium tracking-tight">
--- src/routes/public/HomePage.jsx
+++ src/routes/public/HomePage.jsx
@@ -1,16 +1,17 @@
-import MainNav from '../../components/layout/MainNav'
 
 function HomePage() {
-  return (
-    <div>
-      <MainNav />
-      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-        <h1 className="text-4xl font-bold">Welcome to Vinyl Vibe</h1>
-        <p className="mt-4 text-xl">Discover your next favorite record.</p>
-        {/* Featured products will go here */}
-      </main>
-    </div>
-  )
+	return (
+		<div>
+			<main className="mx-auto max-w-7xl pt-[calc(5rem-1px)]">
+				<div className="px-6 pb-6 pt-40 border">
+					<h1 className="text-5xl font-medium tracking-tight">
+						Vinyl Vibe
+					</h1>
+				</div>
+				{/* Featured products will go here */}
+			</main>
+		</div>
+	);
 }
 
-export default HomePage 
+export default HomePage;
