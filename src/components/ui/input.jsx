11cc3991ca620f6cd60a239acdb2bb6c7b3dde4f
Damian Petrov <hello@damianpetrov.com>
2024-12-13 22:58:33 +1100

    style: input field on hover

--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -3,17 +3,17 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 
 const Input = React.forwardRef(({ className, type, ...props }, ref) => {
-	return (
-		<input
-			type={type}
-			className={cn(
-				"flex h-10 w-full rounded-full border border-border px-4 py-1 text-base transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:border-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-				className
-			)}
-			ref={ref}
-			{...props}
-		/>
-	);
+    return (
+        <input
+            type={type}
+            className={cn(
+                "flex h-10 w-full rounded-full border border-border px-4 py-1 text-base transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground hover:bg-white/70 hover:border-black/15 focus-visible:border-ring focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+                className,
+            )}
+            ref={ref}
+            {...props}
+        />
+    );
 });
 Input.displayName = "Input";
 
