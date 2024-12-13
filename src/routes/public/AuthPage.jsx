f3bc047cbf0cf63cc5e620a24c4f093759ebd2d6
Damian Petrov <hello@damianpetrov.com>
2024-12-14 01:39:34 +1100

    fix: auth page border lineup with nav border

--- src/routes/public/AuthPage.jsx
+++ src/routes/public/AuthPage.jsx
@@ -6,14 +6,16 @@ function AuthPage() {
         <>
             <AuthNav />
 
-            <div className="flex h-full w-full items-center justify-center border-x p-6">
-                <AuthForm />
-            </div>
-            <div className="flex w-full items-center justify-center border p-6">
-                <div className="text-center text-[0.8rem] font-light text-muted-foreground max-w-sm">
-                    By continuing, you agree to Vinyl Vibe's Terms of Service
-                    and Privacy Policy, and to receiving periodic emails with
-                    updates.
+            <div className="flex h-full w-full flex-col items-center justify-center">
+                <div className="flex h-full w-full max-w-7xl items-center justify-center border-x p-6 pt-32">
+                    <AuthForm />
+                </div>
+                <div className="flex w-full max-w-7xl items-center justify-center border p-6">
+                    <div className="max-w-sm text-center text-[0.8rem] font-light text-muted-foreground">
+                        By continuing, you agree to Vinyl Vibe's Terms of
+                        Service and Privacy Policy, and to receiving periodic
+                        emails with updates.
+                    </div>
                 </div>
             </div>
         </>
