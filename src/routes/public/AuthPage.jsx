79fef980b5635b1690538c0477c730b166506293
Damian Petrov <hello@damianpetrov.com>
2024-12-15 20:25:38 +1100

    fix: auth page max width bug

--- src/routes/public/AuthPage.jsx
+++ src/routes/public/AuthPage.jsx
@@ -6,11 +6,11 @@ function AuthPage() {
         <>
             <AuthNav />
 
-            <div className="flex h-full w-full items-center justify-center border-x p-6">
+            <div className="flex h-full w-full mx-auto max-w-7xl items-center justify-center border-x p-6">
                 <AuthForm />
             </div>
-            <div className="flex w-full items-center justify-center border p-6">
-                <div className="text-center text-[0.8rem] font-light text-muted-foreground max-w-sm">
+            <div className="flex w-full max-w-7xl mx-auto items-center justify-center border p-6">
+                <div className="max-w-sm text-center text-[0.8rem] font-light text-muted-foreground">
                     By continuing, you agree to Vinyl Vibe's Terms of Service
                     and Privacy Policy, and to receiving periodic emails with
                     updates.
